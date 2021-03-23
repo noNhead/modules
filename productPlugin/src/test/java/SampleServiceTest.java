417e759f3e7832eee116bfd0506894b34661b552
noNhead <sun495fun@gmail.com>
2021-03-23 21:44:50 +0400

    Two test complete...

--- productPlugin/src/test/java/SampleServiceTest.java
+++ productPlugin/src/test/java/SampleServiceTest.java
@@ -1,11 +1,19 @@
 import entity.Categories;
 import org.junit.Test;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 import service.SampleService;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBException;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
 import java.io.FileNotFoundException;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SampleServiceTest {
     SampleService service = new SampleService();
@@ -18,7 +26,12 @@ public class SampleServiceTest {
     }
 
     @Test
-    public void marshall() throws JAXBException {
-        service.marshall(categories, "productPlugin/src/test-resource/generated/new-categories.xml");
+    public void marshall() throws JAXBException, SAXException {
+        service.marshall(categories, this.getClass().getResource("generated/new-categories.xml").getPath());
+        Source xmlFile = new StreamSource(new File(this.getClass().getResource("generated/new-categories.xml").getPath()));
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(this.getClass().getResource("generated/categories.xsd"));
+        Validator validator = schema.newValidator();
+        assertThrows(SAXParseException.class, () -> validator.validate(xmlFile));
     }
 }
